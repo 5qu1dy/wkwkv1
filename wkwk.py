41f093daf3bfd1bfd009752db899dc0e9100c296
5qu1dy <48722600+5qu1dy@users.noreply.github.com>
2019-04-16 04:30:08 +0700

    Add files via upload

---
+++ wkwk.py
@@ -0,0 +1,102 @@
+#MADE BY MR.5QU1DY
+#DON'T REMAKE!!!
+#WKWKWKWKWK!!!
+
+import os
+import time
+
+os.system ("clear")
+opening='''
+	Welcome to WKWKv1
+	This Script has made by Mr.5qu1d
+	From GARUDA TEAM COMANDO | GTC
+'''
+
+os.system ("toilet W K W K | lolcat -a -d 5")
+time.sleep(0.5)
+print (opening)
+time.sleep(1)
+
+password=input ("	Password : ")
+if password=="w ketawa":
+   time.sleep(1)
+   print ("LOADING...")
+   time.sleep(5)
+   print ("Password Correct!")
+   time.sleep(1)
+   os.system ("clear")
+   os.system ("toilet W K W K | lolcat -a -d 5")
+   time.sleep(1)
+   opening2='''
+        Welcome to WKWKv1
+        This Script has made by Mr.5qu1d
+        From GARUDA TEAM COMANDO | GTC
+   '''
+   print (opening2)
+   menu='''
+           1. Install All Tools
+           2. Show All Tools
+           3. About Me
+
+           4. Close
+   '''
+   menu2='''
+           1. Install Metasploit
+           2. Install A-Rat
+           3. Install Tool-X
+
+           0. Close
+   '''
+   print (menu)
+   choose=input ("     Enter Your Select : ")
+   if choose=="2":
+      os.system ("clear")
+      os.system ("toilet W K W K | lolcat -a -d 5")
+      print (menu2)
+      time.sleep(1)
+      select=input ("     Enter Your Select : ")
+      if select=='1' or select=='01':
+         print ("Installing...")
+         time.sleep(1)
+         os.system ("pkg update && pkg upgrade;pkg install unstable repo; pkg install metasploit")
+      elif select=='2':
+           print ("Installing...")
+           time.sleep(1)
+           os.system ("pkg update && pkg upgrade;pkg install git;pkg install python;pkg install python2;git clone https://github.com/Xi4u7/A-Rat")
+      elif select=='3':
+           print ("Installing...")
+           time.sleep(1)
+           os.system ("pkg update && pkg upgrade;pkg install git;git clone https://github.com/Rajkumrdusad/Tool-X")
+      elif select=='0':
+           print ("Closing...")
+           time.sleep(1)
+      else:
+           print ("ERROR: WRONG INPUT...!!!")
+   elif choose=='1':
+        print ("Installing...")
+        os.system ("pkg update && pkg upgrade;pkg install unstable repo; pkg install metasploit")
+        os.system ("pkg update && pkg upgrade;pkg install git;pkg install python;pkg install python2;git clone https://github.com/Xi4u7/A-Rat")
+        os.system ("pkg update && pkg upgrade;pkg install git;git clone https://github.com/Rajkumrdusad/Tool-X")
+        print (" ")
+        print ("Done...")
+        print ("Have Fun :D")
+   elif choose=="3":
+        os.system ("clear")
+        print ("My Name is Mr.5qu1dy")
+        print ("I am from GARUDA TEAM COMANDO | GTC")
+        print ("THANK YOU!!!")
+        time.sleep(10)
+   elif choose=="4":
+        print ("Closing...")
+        time.sleep(1)
+        os.system ("exit")
+
+   else:
+        print ("ERROR: WRONG INPUT...!!!")
+
+else:
+    time.sleep(1)
+    print ("LOADING...")
+    time.sleep(5)
+    print ("PASSWORD IS WRONG!!!")
+    os.system ("exit")
